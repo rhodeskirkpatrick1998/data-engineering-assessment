f89e6c92a1456ed701403275483de161b3b0c7b6
rhodeskirkpatrick1998 <102641550+rhodeskirkpatrick1998@users.noreply.github.com>
2026-04-25 16:28:43 -0700

    Remove dbt artifacts and add gitignore

--- dbt_shopify/target/compiled/shopify_assessment/models/intermediate/int_order_line_agg.sql
+++
@@ -1,7 +0,0 @@
-select
-    order_id,
-    count(*) as line_item_count,
-    sum(quantity) as total_quantity,
-    sum(quantity * price) as calculated_line_item_amount
-from "airbyte_db"."staging_staging"."stg_order_line_items"
-group by order_id
--- dbt_shopify/target/compiled/shopify_assessment/models/marts/dim_customers.sql
+++
@@ -1,9 +0,0 @@
-select
-    customer_id,
-    admin_graphql_api_id,
-    created_at,
-    tags,
-    addresses,
-    portable_extracted_at,
-    _airbyte_extracted_at
-from "airbyte_db"."staging_staging"."stg_customers"
